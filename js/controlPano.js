b6af151c49c320e0ece7111358264a13a686af85
Katushkins <103944846+katuha1@users.noreply.github.com>
2023-06-27 21:28:49 +0300

    Update controlPano.js

--- js/controlPano.js
+++ js/controlPano.js
@@ -35,26 +35,26 @@ const viewer = new PANOLENS.Viewer({
 });
 
 // Связь в коридоре
-Panorama1.link(Panorama2, SpotPositions[0]);
-Panorama2.link(Panorama1, SpotPositions[1]);
+Panorama1.link(Panorama2, new THREE.Vector3(0, 0, 1500));
+Panorama2.link(Panorama1, new THREE.Vector3(1630, 0, -660));
 
 // Связь коридора_2 и комнаты переговоров_1
-Panorama2.link(Panorama3, SpotPositions[2]);
+Panorama2.link(Panorama3, new THREE.Vector3(1860, 0, 620));
 
 // Связь коридора_1 и комнаты переговоров_1
-Panorama1.link(Panorama3, SpotPositions[3]);
-Panorama3.link(Panorama1, SpotPositions[4]);
+Panorama1.link(Panorama3, new THREE.Vector3(1600, 0, 400));
+Panorama3.link(Panorama1, new THREE.Vector3(-1800, 0, 400));
 
 // Связь переговорной_1 и переговорной_2
-Panorama3.link(Panorama4, SpotPositions[5]);
-Panorama4.link(Panorama3, SpotPositions[6]);
+Panorama3.link(Panorama4, new THREE.Vector3(1600, 0, 0));
+Panorama4.link(Panorama3, new THREE.Vector3(1800, 0, 0));
 
 // Связь коридора_1 и гостинной_1
-Panorama1.link(Panorama5, SpotPositions[7]);
-Panorama5.link(Panorama1, SpotPositions[8]);
+Panorama1.link(Panorama5, new THREE.Vector3(-800, 0, -2400));
+Panorama5.link(Panorama1, new THREE.Vector3(0, 0, 2300));
 
 // Связь гостинной_1 и гостинной_2
-Panorama5.link(Panorama6, SpotPositions[9]);
-Panorama6.link(Panorama5, SpotPositions[10]);
+Panorama5.link(Panorama6, new THREE.Vector3(800, 0, 0));
+Panorama6.link(Panorama5, new THREE.Vector3(-800, 0, 0));
 
 viewer.add(Panorama1, Panorama2, Panorama3, Panorama4, Panorama5, Panorama6);
